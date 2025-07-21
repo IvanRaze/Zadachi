05cb16e2336a3eabfeed08ae7685dd0a1ca79650
IvanRaze <pimshnin@gmail.com>
2025-07-21 14:48:12 +0300

    Create ZadachaVDS3.cs

---
+++ ZadachaVDS3.cs
@@ -0,0 +1,9 @@
+int a = Convert.ToInt32(Console.ReadLine());
+if(a == 5 | a == 10)
+{
+    Console.WriteLine("число равно 5 или равно 10");
+}
+else
+{
+    Console.WriteLine("неизвестное число");
+}
